3d1f1139e64243e43b5eff89b4c9a96b3c110a22
Sajandeep Cheema <sajandeepcheema@gmail.com>
2023-08-12 22:23:33 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,12 +9,15 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-     # path for about view
+    path(route='', view=views.get_dealerships, name='index'),
+
+    # path for about view
     path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
-    
+
+    # path for registration
     path(route='registration/', view=views.registration_request, name='registration'),
 
     # path for login
@@ -23,10 +26,10 @@ urlpatterns = [
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
-
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='dealer/<int:dealer_id>/add_review', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
